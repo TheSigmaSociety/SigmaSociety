0c4b689ac87058e6035e719c3c78e1481d475135
gamedoescool <hi_ishan@outlook.com>
2024-07-10 10:55:39 -0700

    Sigma Stuff

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-document.getElementById('getButton').addEventListener('click', function() {
-	fetch('http://10.0.0.73:5000/get')
-		.then(response => response.json())
-		.then(data => {
-			document.getElementById('response').innerHTML = JSON.stringify(data);
-		})
-});
+// document.getElementById('getButton').addEventListener('click', function() {
+// 	fetch('http://10.0.0.73:5000/get')
+// 		.then(response => response.json())
+// 		.then(data => {
+// 			document.getElementById('response').innerHTML = JSON.stringify(data);
+// 		})
+// });
 
-document.getElementById('postButton').addEventListener('click', function() {
-	fetch('http://10.0.0.73:5000/post', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({key: 'value'}),
-	})
-	.then(response => response.json())
-	.then(data => {
-		document.getElementById('response').innerHTML = JSON.stringify(data);
-	})
-});
+// document.getElementById('postButton').addEventListener('click', function() {
+// 	fetch('http://10.0.0.73:5000/post', {
+// 		method: 'POST',
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 		body: JSON.stringify({key: 'value'}),
+// 	})
+// 	.then(response => response.json())
+// 	.then(data => {
+// 		document.getElementById('response').innerHTML = JSON.stringify(data);
+// 	})
+// });
+//function that gets called apon clicking login
+function logIn() {
+	document.getElementById('response').innerHTML = ""
+	console.log("logging in!")
+	userName = document.getElementById("uname").value
+	password = document.getElementById('psw').value
+
+}
