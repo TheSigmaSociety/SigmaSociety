c24e217850e749f14052038eb9fc7a0eefb7c01b
gamedoescool <hi_ishan@outlook.com>
2024-07-10 12:16:19 -0700

    added sigma things

--- app.js
+++ app.js
@@ -20,10 +20,18 @@
 // 	})
 // });
 //function that gets called apon clicking login
+IP = "http://127.0.0.1:5000" // add your ip along w/ port
 function logIn() {
 	document.getElementById('response').innerHTML = ""
 	console.log("logging in!")
 	userName = document.getElementById("uname").value
 	password = document.getElementById('psw').value
 
+	fetch(IP + "/post", {
+		method: "POST",
+		headers: {"Content-Type":"application/json"},
+		body: JSON.stringify({"username":userName,"password":password})
+	}).then(response => response.json()).then(data => {
+		document.getElementById("response").innerHTML = JSON.stringify(data)
+	})
 }
--- loginScript.py
+++ loginScript.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+
+accounts = {"sigma":"123","tejas":"panja"}
+
 @app.route('/get', methods=['GET'])
 def testget():
     return jsonify(message='get request received')
@@ -9,7 +12,11 @@ def testget():
 @app.route('/post', methods=['POST'])
 def testpost():
     content = request.json
-    return jsonify(received=True, content=content)
+    if(content['username'] not in accounts.keys()):
+        return jsonify(status = "Invalid username")
+    if(accounts[content["username"]] != content["password"]):
+        return jsonify(status="Invalid Password.")
+    return jsonify(content= "logged in as " + content['username'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
